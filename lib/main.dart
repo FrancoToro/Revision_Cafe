b0ecc91b2a28364a5489c56f4bab8e94ed06c3e9
Agustin-Venegas <68612141+Agustin-Venegas@users.noreply.github.com>
2024-11-12 00:34:35 -0300

    iconos y database

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,14 @@ import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+import 'package:cafemixes/utils/DatabaseHelper.dart';
+
 void main() async
 {
   //iniciar y/o abrir la base de datos
   WidgetsFlutterBinding.ensureInitialized();
 
-  final database = openDatabase(
-  join(await getDatabasesPath(),"coffee_db"),
-  onCreate: (db, version) {
-    return db.execute("CREATE TABLE recipes(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, descripcion TEXT, ingredientes TEXT, pasos TEXT, favorita INTEGER)");
-  }, version: 1,
-  );
+  DatabaseHelper.InitDatabase();
 
   runApp(const MyApp());
 }
@@ -26,8 +23,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
       title: 'CafeMixes',
       home: const MyHomePage(),
--- lib/utils/DatabaseHelper.dart
+++ lib/utils/DatabaseHelper.dart
@@ -2,3 +2,18 @@ import 'dart:async';
 
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper 
+{
+    static final database;
+
+    static void InitDatabase()
+    {
+        database = openDatabase(
+            join(await getDatabasesPath(),"coffee_db"),
+            onCreate: (db, version) {
+                return db.execute("CREATE TABLE recipes(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, descripcion TEXT, ingredientes TEXT, pasos TEXT, favorita INTEGER)");
+                }, version: 1,
+            );
+    }
+}
